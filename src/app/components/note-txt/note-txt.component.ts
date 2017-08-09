36eb4c697e790aab86d5a24ba3a29793560fe112
patriki <patriki@MacBook-Pro-de-patriki.local>
2017-08-09 17:13:34 +0200

    images folder

--- src/app/components/note-txt/note-txt.component.ts
+++ src/app/components/note-txt/note-txt.component.ts
@@ -11,7 +11,11 @@ export class NoteTxtComponent implements OnInit {
   noteTxts: Array<Object> = [
     { id: 100, title: 'la compra del super', contentNote: ' acuerdate de comprar aguacates y leche. Tambien tomates' },
     { id: 201, title: 'nota2', contentNote: 'content of note2' },
-    { id: 302, title: 'nota3', contentNote: 'content of note3' }
+    { id: 302, title: 'nota3', contentNote: 'content of note3' },
+    { id: 431, title: 'nota4', contentNote: 'content of note4' },
+    { id: 352, title: 'nota5', contentNote: 'content of note5' },
+    { id: 261, title: 'nota6', contentNote: 'content of note6' },
+    { id: 372, title: 'nota7', contentNote: 'content of content of content of content omprar aguacates y leche. Tambien tomatesn of note7' }
   ];
 
 
